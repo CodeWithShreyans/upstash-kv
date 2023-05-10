d0c5ca22fd6696a2b8517d439ba5cedb91cd2ba4
Shreyans Jain <shreyansthebest2007@gmail.com>
2023-05-11 02:21:22 +0530

    use @vercel/kv as dependency
    Signed-off-by: Shreyans Jain <shreyansthebest2007@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,8 @@
 module.exports = {
-  root: true,
-  extends: [
-    require.resolve('@vercel/style-guide/eslint/node'),
-    require.resolve('@vercel/style-guide/eslint/typescript'),
-  ],
-  ignorePatterns: ['packages/*/dist/**'],
-  overrides: [
-    {
-      files: ['**/*.test.ts'],
-      extends: [require.resolve('@vercel/style-guide/eslint/jest')],
-    },
-  ],
+    root: true,
+    extends: [
+        require.resolve("@vercel/style-guide/eslint/node"),
+        require.resolve("@vercel/style-guide/eslint/typescript"),
+    ],
+    ignorePatterns: ["packages/*/dist/**"],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,27 @@
-import { Redis, type RedisConfigNodejs } from "@upstash/redis";
-import { type ScanCommandOptions } from "@upstash/redis/types/pkg/commands/scan";
+import { createClient } from "@vercel/kv";
 
-let _kv: Redis | null = null;
-process.env.UPSTASH_DISABLE_TELEMETRY = "1";
-
-export class UpstashKV extends Redis {
-    // This API is based on https://github.com/redis/node-redis#scan-iterator which is not supported in @upstash/redis
-    /**
-     * Same as `scan` but returns an AsyncIterator to allow iteration via `for await`.
-     */
-    async *scanIterator(options?: ScanCommandOptions): AsyncIterable<string> {
-        let cursor = 0;
-        let keys: string[];
-        do {
-            // eslint-disable-next-line no-await-in-loop
-            [cursor, keys] = await this.scan(cursor, options);
-            for (const key of keys) {
-                yield key;
-            }
-        } while (cursor !== 0);
+export default function create() {
+    if (
+        !process.env.UPSTASH_REDIS_REST_URL &&
+        !process.env.UPSTASH_REDIS_REST_TOKEN
+    ) {
+        throw new Error(
+            "upstash-kv: Missing required environment variables UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN"
+        );
+    } else if (!process.env.UPSTASH_REDIS_REST_URL) {
+        throw new Error(
+            "upstash-kv: Missing required environment variable UPSTASH_REDIS_REST_URL"
+        );
+    } else if (!process.env.UPSTASH_REDIS_REST_TOKEN) {
+        throw new Error(
+            "upstash-kv: Missing required environment variable UPSTASH_REDIS_REST_TOKEN"
+        );
     }
 
-    /**
-     * Same as `hscan` but returns an AsyncIterator to allow iteration via `for await`.
-     */
-    async *hscanIterator(
-        key: string,
-        options?: ScanCommandOptions
-    ): AsyncIterable<string | number> {
-        let cursor = 0;
-        let items: (number | string)[];
-        do {
-            // eslint-disable-next-line no-await-in-loop
-            [cursor, items] = await this.hscan(key, cursor, options);
-            for (const item of items) {
-                yield item;
-            }
-        } while (cursor !== 0);
-    }
-
-    /**
-     * Same as `sscan` but returns an AsyncIterator to allow iteration via `for await`.
-     */
-    async *sscanIterator(
-        key: string,
-        options?: ScanCommandOptions
-    ): AsyncIterable<string | number> {
-        let cursor = 0;
-        let items: (number | string)[];
-        do {
-            // eslint-disable-next-line no-await-in-loop
-            [cursor, items] = await this.sscan(key, cursor, options);
-            for (const item of items) {
-                yield item;
-            }
-        } while (cursor !== 0);
-    }
-
-    /**
-     * Same as `zscan` but returns an AsyncIterator to allow iteration via `for await`.
-     */
-    async *zscanIterator(
-        key: string,
-        options?: ScanCommandOptions
-    ): AsyncIterable<string | number> {
-        let cursor = 0;
-        let items: (number | string)[];
-        do {
-            // eslint-disable-next-line no-await-in-loop
-            [cursor, items] = await this.zscan(key, cursor, options);
-            for (const item of items) {
-                yield item;
-            }
-        } while (cursor !== 0);
-    }
+    return createClient({
+        url: process.env.UPSTASH_REDIS_REST_URL,
+        token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    });
 }
 
-export function createClient(config: RedisConfigNodejs): UpstashKV {
-    return new UpstashKV(config);
-}
-
-// eslint-disable-next-line import/no-default-export
-export default new Proxy(
-    {},
-    {
-        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-        get(target, prop) {
-            if (!_kv) {
-                if (
-                    !process.env.UPSTASH_REST_API_URL ||
-                    !process.env.UPSTASH_REST_API_TOKEN
-                ) {
-                    throw new Error(
-                        "@vercel/kv: Missing required environment variables UPSTASH_REST_API_URL and UPSTASH_REST_API_TOKEN"
-                    );
-                }
-
-                _kv = createClient({
-                    url: process.env.UPSTASH_REST_API_URL,
-                    token: process.env.UPSTASH_REST_API_TOKEN,
-                });
-            }
-
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-            return Reflect.get(_kv, prop);
-        },
-    }
-) as UpstashKV;
+export { createClient };
